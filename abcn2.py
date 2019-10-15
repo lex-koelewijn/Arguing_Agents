79eef6cab520d2d5424b39095491eada1a903805
Joppe Boekestijn <joppeboekestijn@gmail.com>
2019-10-15 13:29:22 +0200

    to-do list updated

--- abcn2.py
+++ abcn2.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.4'
-#       jupytext_version: 1.2.4
+#       jupytext_version: 1.2.3
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
